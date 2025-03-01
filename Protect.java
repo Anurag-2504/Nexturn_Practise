392fc179b0094aa56b83c084a681a320ad8e480a
Anurag-2504 <anuragratna2003@gmail.com>
2025-03-01 11:46:57 +0530

    Protected Concept

---
+++ Protect.java
@@ -0,0 +1,6 @@
+public class Protect {
+    protected void show(){
+        System.out.println("Hello I am in Protect Class");
+    }
+    
+}
---
+++ ProtectMain.java
@@ -0,0 +1,7 @@
+public class ProtectMain {
+    public static void main(String[] args) {
+        Protect obj=new Protect();
+        obj.show();
+    }
+    
+}
